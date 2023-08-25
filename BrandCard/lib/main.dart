3153f1a0901b1351a3403d6531529db2a22b3202
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-08-24 19:58:54 -0600

    deck page build

--- BrandCard/lib/main.dart
+++ BrandCard/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:js';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 // Entry point of the app
@@ -34,26 +35,68 @@ class MyApp extends StatelessWidget {
 // S T A T E  M A N A G M E N T
 class MyAppState extends ChangeNotifier {
   List<BrandCard> cards = [
-    BrandCard(type: 'brand', name: 'DEFENDER', description: 'Knight, Superhero, Warrior'),
+    BrandCard(
+        type: 'brand',
+        name: 'DEFENDER',
+        description: 'Knight, Superhero, Warrior'),
     BrandCard(type: 'brand', name: 'EXPLORER', description: 'Seeker, Wanderer'),
-    BrandCard(type: 'brand', name: 'THRILL SEEKER', description: 'Gambler, Swashbuckler, Adventurer'),
-    BrandCard(type: 'brand', name: 'ACHIEVER', description: 'Athlete, Hot Shot, Strongman'),
-    BrandCard(type: 'brand', name: 'TRADITIONALIST', description: 'Conservative, Old School, Miser'),
-    BrandCard(type: 'brand', name: 'NURTURER', description: 'Mom, Mother Earth, Healer'),
-    BrandCard(type: 'brand', name: 'CONNECTOR', description: 'Networker, Politician, Talker'),
-    BrandCard(type: 'brand', name: 'ARTIST', description: 'Creative, Creator, Crafstman'),
-    BrandCard(type: 'brand', name: 'PHILOSOPHER', description: 'Sage, Prophet, Guru'),
-    BrandCard(type: 'brand', name: 'DREAMER', description: 'Magician, Sorcerer, Wizard'),
-    BrandCard(type: 'brand', name: 'MOTIVATOR', description: 'Mentor, Preacher, Promoter'),
-    BrandCard(type: 'brand', name: 'RULER', description: 'King, Leader, Father'),
-    BrandCard(type: 'brand', name: 'MAVERICK', description: 'Rebel, Outlaw, Rogue'),
-    BrandCard(type: 'brand', name: 'EVERYONE', description: 'Average Joe, Girl Next Door'),
-    BrandCard(type: 'brand', name: 'ENTERTAINER', description: 'Clown, Jester, Preformer'),
-    BrandCard(type: 'brand', name: 'VILLIAN', description: 'Bad Guy, Monster, Vampire'),
-    BrandCard(type: 'brand', name: 'Intellectual', description: 'Sage, Genius, Expert'),
+    BrandCard(
+        type: 'brand',
+        name: 'THRILL SEEKER',
+        description: 'Gambler, Swashbuckler, Adventurer'),
+    BrandCard(
+        type: 'brand',
+        name: 'ACHIEVER',
+        description: 'Athlete, Hot Shot, Strongman'),
+    BrandCard(
+        type: 'brand',
+        name: 'TRADITIONALIST',
+        description: 'Conservative, Old School, Miser'),
+    BrandCard(
+        type: 'brand',
+        name: 'NURTURER',
+        description: 'Mom, Mother Earth, Healer'),
+    BrandCard(
+        type: 'brand',
+        name: 'CONNECTOR',
+        description: 'Networker, Politician, Talker'),
+    BrandCard(
+        type: 'brand',
+        name: 'ARTIST',
+        description: 'Creative, Creator, Crafstman'),
+    BrandCard(
+        type: 'brand', name: 'PHILOSOPHER', description: 'Sage, Prophet, Guru'),
+    BrandCard(
+        type: 'brand',
+        name: 'DREAMER',
+        description: 'Magician, Sorcerer, Wizard'),
+    BrandCard(
+        type: 'brand',
+        name: 'MOTIVATOR',
+        description: 'Mentor, Preacher, Promoter'),
+    BrandCard(
+        type: 'brand', name: 'RULER', description: 'King, Leader, Father'),
+    BrandCard(
+        type: 'brand', name: 'MAVERICK', description: 'Rebel, Outlaw, Rogue'),
+    BrandCard(
+        type: 'brand',
+        name: 'EVERYONE',
+        description: 'Average Joe, Girl Next Door'),
+    BrandCard(
+        type: 'brand',
+        name: 'ENTERTAINER',
+        description: 'Clown, Jester, Preformer'),
+    BrandCard(
+        type: 'brand',
+        name: 'VILLIAN',
+        description: 'Bad Guy, Monster, Vampire'),
+    BrandCard(
+        type: 'brand',
+        name: 'Intellectual',
+        description: 'Sage, Genius, Expert'),
   ];
 
-  late BrandCard current ;
+  late BrandCard current;
 
   BrandCard getRandomCard() {
     final availableCards =
@@ -64,25 +107,24 @@ class MyAppState extends ChangeNotifier {
       current = availableCards[randomIndex];
       return current;
     } else {
-      if(cards.isNotEmpty){
+      if (cards.isNotEmpty) {
         final lastCard = cards.removeAt(0);
         favorites.add(lastCard);
         return lastCard;
       }
-      if(availableCards.isEmpty){
+      if (availableCards.isEmpty) {
         navToFavorites(context as BuildContext);
-        
       }
       return availableCards.first;
-      }
-      }
+    }
+  }
 
   var favorites = <BrandCard>[]; // Storing favorites
 
-  void navToFavorites(BuildContext context){
+  void navToFavorites(BuildContext context) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context)=> FavoritesPage()),
+      MaterialPageRoute(builder: (context) => FavoritesPage()),
     );
   }
 
@@ -94,9 +136,8 @@ class MyAppState extends ChangeNotifier {
       favorites.add(current);
     }
     notifyListeners(); // Notifying listeners (UI) that the state has changed
-    }
   }
-
+}
 
 // H O M E  P A G E / L A N D I N G  P A G E
 class MyHomePage extends StatefulWidget {
@@ -117,9 +158,9 @@ class _MyHomePageState extends State<MyHomePage> {
       case 1:
         page = FavoritesPage();
         break;
-      // case 2:
-      //   page = AllCards();
-      //   break;
+      case 2:
+        page = AllCardsPage(appState: MyAppState(),deck: [],);
+        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
@@ -169,7 +210,6 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class GeneratorPage extends StatefulWidget {
- 
   final MyAppState appState;
 
   GeneratorPage({required this.appState});
@@ -190,7 +230,6 @@ class _GeneratorPageState extends State<GeneratorPage> {
   bool chooseCard2 = true;
   bool finalView = false;
 
-  
   @override
   void initState() {
     super.initState();
@@ -200,86 +239,89 @@ class _GeneratorPageState extends State<GeneratorPage> {
     card2 = appState.getRandomCard();
   }
 
-
   Widget buildError(BuildContext context) {
     throw UnimplementedError();
   }
 
   @override
   Widget build(BuildContext context) {
-    if(cards.length == 2){
-      finalView==true;
+    if (cards.length == 2) {
+      finalView == true;
     }
-    if (finalView == true){
+    if (finalView == true) {
       return Center(
-        child: CardZone(card: winningCard ),
+        child: CardZone(card: winningCard),
       );
-    } else{
-
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    chooseCard1 = true;
-                    if (chooseCard1) {
-                      cards.remove(card2);
-                      cards.remove(card1);
-                    }
-                    card2 = appState.getRandomCard();
-                    while( card1 == card2 ){
-                    card2 = appState.getRandomCard();
-                    }
-                    List<BrandCard> availableCards = appState.cards;
-                    if(cards.length == 2){
-                      finalView = true;
-                      winningCard = card1;
-                    }
-              //TERMINAL LOG      
-                    print(cards.length);
-                    for (var card in cards) {
-                      print('${card.name}');
-                    }
-                  });
-                },
-                child: CardZone(
-                  card: card1,
+    } else {
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      // C A R D   C H O I C E
+                      chooseCard1 = true;
+                      if (chooseCard1) {
+                        // R E M O V E   C A R D S   F R O M   D E C K
+                        cards.remove(card2);
+                        cards.remove(card1);
+                      }
+                      // G E N E R A T E   N E W   C A R D
+                      card2 = appState.getRandomCard();
+                      // I F   R E P E A T E D   C A R D   G E T   N E W
+                      while (card1 == card2) {
+                        card2 = appState.getRandomCard();
+                      }
+                      card1.isClicked = true;
+                      // S H O W   W I N N I N G   C A R D
+                      if (cards.length == 1) {
+                        finalView = true;
+                        winningCard = card1;
+                      }
+                      //############## LOG
+                      print(cards.length);
+                      for (var card in cards) {
+                        print('${card.name}');
+                      }
+                    });
+                  },
+                  child: CardZone(
+                    card: card1,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    chooseCard2 = true;
-                    if(chooseCard2){
-                      cards.remove(card1);
-                    }
-                    card1 = appState.getRandomCard();
-                    while(card2 == card1){
+              SizedBox(width: 10),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      chooseCard2 = true;
+                      if (chooseCard2) {
+                        cards.remove(card1);
+                      }
                       card1 = appState.getRandomCard();
-                    }
-                    List<BrandCard> availableCards = appState.cards;
-                    if(cards.length == 2){
-                      finalView = true;
-                      winningCard = card2;
-                    }
-                  });
-                },
-                child: CardZone(
-                  card: card2,
+                      while (card2 == card1) {
+                        card1 = appState.getRandomCard();
+                      }
+                      card2.isClicked = true;
+                      if (cards.length == 1) {
+                        finalView = true;
+                        winningCard = card2;
+                      }
+                    });
+                  },
+                  child: CardZone(
+                    card: card2,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
     }
   }
 }
@@ -290,49 +332,63 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
+    var clickedCards = appState.cards.where((card) => card.isClicked).toList();
+
+    if (clickedCards.isEmpty) {
       return Center(
-        child: Text('No Favorites yet.'),
+        child: Text('No Cards Yet'),
       );
     }
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.name),
-          ),
-      ],
+    print(clickedCards.length);
+    return ListView.builder(
+      itemCount: clickedCards.length,
+      itemBuilder: (context, index) {
+        var card = clickedCards[index];
+        return ListTile(
+          leading: Icon(Icons.favorite),
+          title: Text(card.name),
+        );
+      },
     );
   }
 }
 
 // T O  D O :  c r e a t e  p a g e  f o r  a l l  b r a n d  t y p e s
-// Class AllCardsPage extends StatelessWidget {
-//   var card = Cards;
+class AllCardsPage extends StatelessWidget {
+  final MyAppState appState;
+  final List<BrandCard> deck;
 
-//   @override
-//   Widget build(BuildContext context){
-//     return Center (
-//       child: Text('${card.name}'),
-//     );
-//   }
+  const AllCardsPage({super.key, required this.appState, required this.deck});
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('CARDS'),
+        ),
+        body: ListView.builder(
+            itemCount: deck.length,
+            itemBuilder: (context, index) {
+              var card = deck[index];
+              return CardZone(card: card);
+            }));
+  }
+}
 
 // B R A N D   C A R D
+// ignore: must_be_immutable
 class BrandCard extends StatelessWidget {
   final String type;
   final String name;
   final String description;
+  bool isClicked;
 
-  const BrandCard({
+  BrandCard({
     required this.type,
     required this.name,
     required this.description,
+    this.isClicked = false,
     Key? key,
   }) : super(key: key);
 
@@ -353,7 +409,7 @@ class BrandCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  'Name: $name',
+                  '$name',
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
